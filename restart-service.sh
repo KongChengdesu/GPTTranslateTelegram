18c2e694af87a587ad740634b1406eec258a7af6
KongChengdesu <77406643+KongChengdesu@users.noreply.github.com>
2024-05-07 16:24:07 -0700

    Added autorun

---
+++ restart-service.sh
@@ -0,0 +1,12 @@
+# resetservice.sh
+#!/bin/bash
+pm2 stop GPTTranslateTelegram
+pm2 delete GPTTranslateTelegram
+pm2 save
+rm -rf ~/apps/GPTTranslateTelegram
+mkdir ~/apps/GPTTranslateTelegram
+cp -r ~/actions-runner/_work/GPTTranslateTelegram/GPTTranslateTelegram/* ~/apps/GPTTranslateTelegram
+cp ~/translate.env ~/apps/GPTTranslateTelegram/.env
+cd ~/apps/GPTTranslateTelegram
+pm2 start ecosystem.config.js
+pm2 save
