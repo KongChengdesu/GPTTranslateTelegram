383d9f985b3d3aa3c0fbe1f0480f8c0ffc6f26db
KongChengdesu <77406643+KongChengdesu@users.noreply.github.com>
2024-05-07 17:10:01 -0700

    Update restart-service.sh

--- restart-service.sh
+++ restart-service.sh
@@ -5,7 +5,7 @@ pm2 delete GPTTranslateTelegram
 pm2 save
 rm -rf ~/apps/GPTTranslateTelegram
 mkdir ~/apps/GPTTranslateTelegram
-cp -r ~/actions-runner/_work/GPTTranslateTelegram/GPTTranslateTelegram/* ~/apps/GPTTranslateTelegram
+cp -r ~/translate-runner/_work/GPTTranslateTelegram/GPTTranslateTelegram/* ~/apps/GPTTranslateTelegram
 cp ~/translate.env ~/apps/GPTTranslateTelegram/.env
 cd ~/apps/GPTTranslateTelegram
 pm2 start ecosystem.config.js
