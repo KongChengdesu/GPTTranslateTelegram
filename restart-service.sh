707efd7e5c08b37a9de80ff6d1f920ebe8e841d3
KongChengdesu <77406643+KongChengdesu@users.noreply.github.com>
2024-07-02 11:20:47 -0700

    Update restart-service.sh

--- restart-service.sh
+++ restart-service.sh
@@ -4,6 +4,7 @@ pm2 stop GPTTranslateTelegram
 pm2 delete GPTTranslateTelegram
 pm2 save
 rm -rf ~/apps/GPTTranslateTelegram
+mkdir ~/apps
 mkdir ~/apps/GPTTranslateTelegram
 cp -r ~/translate-runner/_work/GPTTranslateTelegram/GPTTranslateTelegram/* ~/apps/GPTTranslateTelegram
 cp ~/translate.env ~/apps/GPTTranslateTelegram/.env
